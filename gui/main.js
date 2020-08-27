d7a4f8a039b47847a3ceb0d42c2fc59017684200
Omid <omid@sabz.city>
2020-08-27 19:03:26 +0430

    Improve GUI main file architecture >> - Move splash landing page to landings from main.html - Fix first time see webapp to respect given lang in URL - Just respect user preferences home page in PWA installed version

--- gui/main.js
+++ gui/main.js
@@ -9,13 +9,14 @@ import './libjs/gui-engine/application.js'
 // init()
 
 // function main() {
-var script = document.createElement('script')
-const lang = Application.UserPreferences.ContentPreferences.Language.iso639_1 || "en"
-script.src = "/init-" + lang + ".js"
-document.head.appendChild(script)
-script.onload = function () {
-    // First check user preference
-    if (Application.UserPreferences.HomePage !== "" && window.location.pathname === "/") {
+const lang = Application.UserPreferences.ContentPreferences.Language.iso639_1 || Polyfill.GetLangRegFromURL().l || "en"
+
+const initScript = document.createElement('script')
+initScript.src = "/init-" + lang + ".js"
+document.head.appendChild(initScript)
+initScript.onload = function () {
+    // First check user preference in PWA version
+    if (!window.matchMedia('(display-mode: browser)').matches && window.location.pathname === "/") {
         window.history.replaceState({}, "", "/" + Application.UserPreferences.HomePage)
         Application.Router(Application.UserPreferences.HomePage, "")
     } else {
